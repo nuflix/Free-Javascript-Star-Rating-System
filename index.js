235a71f25bc4dad9f275eb05ad0d5da94d7c3b67
nuflix <djordje.vujicic@yandex.com>
2020-09-05 14:25:36 +0200

    background stars

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ for(let i=0; i<obj.length; i++){
     document.getElementsByClassName(className)[i].parentElement.style.width = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
     document.getElementsByClassName(className)[i].parentElement.style.height = parseInt(obj[i].starSize)+"px";
 
+    if(obj[i].backgroundStarImage){
+        console.log("a");
+        document.getElementsByClassName(className)[i].parentElement.style.backgroundSize=obj[i].starSize + "px";
+        document.getElementsByClassName(className)[i].parentElement.style.backgroundRepeat="repeat-x";
+        document.getElementsByClassName(className)[i].parentElement.style.backgroundImage = "url('" + obj[i].backgroundStarImage + "')" ;
+    }
+
     if(obj[i].emptyStarImage){
     document.getElementsByClassName(className)[i].innerHTML = '<div class="emptyStarRating"></div>';
     document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundSize = parseInt(obj[i].starSize) + "px";
